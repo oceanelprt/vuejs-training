ef34fdc99952d1f9f0bf6e64c4eb2e6e63de45ed
olaporte <oceane.laporte1@gmail.com>
2023-06-17 15:59:07 +0200

    Interpolation

--- vue.js/post/src/components/New-onef.vue
+++ vue.js/post/src/components/New-onef.vue
@@ -1,10 +1,26 @@
 <template>
-    <h1>Hello there</h1>
+    <h1>Hello there {{ "hello".length }}</h1>
+    <h1>Email {{ email }}</h1>
+    <h1>Number {{ mobile = 111 }}</h1>
+    <h1>Name {{ getName1() }}</h1>
+    <h1>Name {{ getName2("Helio") }}</h1>
 </template>
 
 <script>
     export default{
-        name:"PAGES"
+        name: "PAGES",
+        data() {
+            return {
+                email: "abcd@gmail.com",
+                mobile: "55555",
+                getName1: function(){
+                    return "Oceane";
+                },
+                getName2: function(name){
+                    return name;
+                }
+            }
+        }
     }
 </script>
 
