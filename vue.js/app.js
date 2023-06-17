0e7171cf60bbcfa966237a07a6ba03c4a970931d
olaporte <oceane.laporte1@gmail.com>
2023-06-17 13:21:49 +0200

    Using VueJS without installation

---
+++ vue.js/app.js
@@ -0,0 +1,13 @@
+// Affiche un warning
+console.warn("Hello world");
+
+// Affiche une erreur 
+console.error("Hello world")
+
+// Clear la console
+console.clear("Hello world");
+
+var fun = Vue.createApp({
+    template: "<h1>Hello Everyone</h1>",
+});
+fun.mount("#main");
