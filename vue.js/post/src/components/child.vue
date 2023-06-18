daac2af6b51345e7e3be2b66ae3c34a44af8357f
olaporte <oceane.laporte1@gmail.com>
2023-06-18 12:00:12 +0200

    Add child component and pass data to it

---
+++ vue.js/post/src/components/child.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1 id="page-child">Child component</h1>
+    <h3>{{ name }}</h3>
+    <h3>{{ userTony.email }}</h3>
+    <button v-on:click="getData()">Call the function</button>
+</template>
+
+<script>
+    export default {
+        name: "child-component",
+        props: {
+            name: String,
+            userTony: Object,
+            getData: Function,
+        }
+    };
+</script>
+
+<style scoped>
+    h3 {
+        color:blue;
+        font-weight: bold;
+    }
+</style>
