ab1f8d500fd978914af5cbc321e0dcb84028e86b
olaporte <oceane.laporte1@gmail.com>
2023-06-18 14:12:41 +0200

    Passing data from child component to parent

---
+++ vue.js/post/src/components/school.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>School</h1>
+    <h1>{{ childUser }}</h1>
+    <TeacherComponent name="Bruce" :getUser="getUserName"/>
+    <StudentComponent name="Leo"/>
+</template>
+
+<script>
+    import StudentComponent from './student.vue'
+    import TeacherComponent from './teacher.vue'
+
+    export default {
+        name: "school-component",
+        components: {
+            StudentComponent,
+            TeacherComponent,
+        },
+        methods: {
+            getUserName(name) {
+                this.childUser = name;
+            },
+        },
+        data() {
+            return {
+                childUser: "",
+            }
+        }
+    };
+</script>
---
+++ vue.js/post/src/components/teacher.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Teacher: {{ name }}</h1>
+    <button v-on:click="getUser(userName)">Get the name</button>
+</template>
+
+<script>
+   export default {
+       name: "teacher-component",
+       props: {
+            name: String,
+            getUser: Function,
+       },
+       data() {
+        return {
+            userName: "Clark",
+        }
+       }
+   };
+</script>
