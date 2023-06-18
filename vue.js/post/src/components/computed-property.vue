2a5e3ea71f6aa5e5cadfed94f3c9cd7ba4c7d46b
olaporte <oceane.laporte1@gmail.com>
2023-06-18 18:17:53 +0200

    Computed property

---
+++ vue.js/post/src/components/computed-property.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Computed property</h1>
+    <!-- <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span> -->
+    <span>{{ getTotal }}</span>
+</template>
+
+<script>
+    export default{
+        name: "computed-property-component",
+        data() {
+            return {
+                // author : {
+                //     name: 'John Doe',
+                //     books: [
+                //         'Vue 2 - Advanced Guide',
+                //         'Vue 3 - Basic Guide',
+                //         'Vue 4 - The Mystery'
+                //     ],
+                // },
+                cashinhand: 200,
+                balance: 50,
+                discount: 160,
+            };
+        },
+        computed: {
+        //     // a computed getter
+        //     publishedBooksMessage() {
+        //         // 'this' points to the component instance
+        //         return this.author.books.length > 0 ? "Yes" : "No";
+        //     },
+            getTotal(){
+                return this.cashinhand * this.balance - this.discount;
+            },
+        }
+    }
+</script>
