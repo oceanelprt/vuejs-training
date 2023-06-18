e696653c92ea2183bd7c9509bd0d7e75e9b5d4b2
olaporte <oceane.laporte1@gmail.com>
2023-06-18 17:07:44 +0200

    Complete form

--- vue.js/post/src/components/form.vue
+++ vue.js/post/src/components/form.vue
@@ -4,8 +4,26 @@
         <label for="">Username</label>&nbsp;
         <input type="text" name="" id="" placeholder="username" v-model="form.username">
         <br/>
-        <label for="">Password</label>&nbsp;
+        <br/>
+        <label for="">Password</label>&nbsp;&nbsp;
         <input type="password" name="" id="" v-model="form.password">
+        <br/>
+        <br/>
+        <label for="">Hobbies</label>&nbsp;&nbsp;
+        <label for="basketball">Basketball</label>&nbsp;&nbsp;
+        <input type="checkbox" name="" id="basketball" value="basketball" v-model="form.hobbies">
+        <label for="horse-riding">Horse riding</label>&nbsp;&nbsp;
+        <input type="checkbox" name="" id="horse-riding" value="horse-riding" v-model="form.hobbies">
+        <label for="golf">Golf</label>&nbsp;&nbsp;
+        <input type="checkbox" name="" id="golf" value="golf" v-model="form.hobbies">
+        <br/>
+
+        <label for="">Gender</label>&nbsp;&nbsp;
+        <label for="">Male</label>&nbsp;&nbsp;
+        <input type="radio" name="gender" id="" value="male" v-model="form.gender">
+        <label for="">Female</label>&nbsp;&nbsp;
+        <input type="radio" name="gender" id="" value="female" v-model="form.gender">
+        <br/>
         <br/>
         <button v-on:click="login()">Sign up</button>
 
@@ -21,6 +39,8 @@
                 form : {
                     username:"",
                     password:"",
+                    hobbies: [],
+                    gender: "",
                 }
             }
         },
