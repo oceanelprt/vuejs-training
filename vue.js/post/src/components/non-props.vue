d8c70b28b1a310227677129541b4299411828335
olaporte <oceane.laporte1@gmail.com>
2023-06-18 17:59:15 +0200

    Props & non-props

---
+++ vue.js/post/src/components/non-props.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Non props</h1>
+    <h2>{{ info }}</h2>
+        <div v-bind="$attrs">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae velit minima alias eius error reprehenderit totam, obcaecati maxime provident quae, consequuntur architecto quo! Minima nisi pariatur excepturi itaque, sapiente autem. </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae velit minima alias eius error reprehenderit totam, obcaecati maxime provident quae, consequuntur architecto quo! Minima nisi pariatur excepturi itaque, sapiente autem. </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae velit minima alias eius error reprehenderit totam, obcaecati maxime provident quae, consequuntur architecto quo! Minima nisi pariatur excepturi itaque, sapiente autem. </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae velit minima alias eius error reprehenderit totam, obcaecati maxime provident quae, consequuntur architecto quo! Minima nisi pariatur excepturi itaque, sapiente autem. </p>
+            <h2>{{ info }}</h2>
+        </div>
+</template>
+
+<script>
+    export default{
+        name: "non-props-component",
+        props: {
+            info: String,
+        },
+    }
+</script>
---
+++ vue.js/post/src/components/props.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Props</h1>
+    <NonPropsComponent info="Hello World" id="name" />
+</template>
+
+<script>
+    import NonPropsComponent from './non-props.vue'
+
+    export default{
+        name: "props-component",
+        components:{
+            NonPropsComponent,
+        }
+    }
+</script>
