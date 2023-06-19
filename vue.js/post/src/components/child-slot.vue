058183aed8dcb825beed362d2761fa729e8d4f8e
olaporte <oceane.laporte1@gmail.com>
2023-06-19 17:40:10 +0200

    Dynamic components

--- vue.js/post/src/components/child-slot.vue
+++ vue.js/post/src/components/child-slot.vue
@@ -1,6 +1,11 @@
 <template>
     <h1>Slots</h1>
-    <slot></slot>
+    <div id="product-card">
+        <header><slot name="header"></slot></header>
+        <section><slot name="section"></slot></section>
+        <main><slot name="main"></slot></main>
+        <footer><slot name="footer"></slot></footer>
+    </div>
 </template>
 
 <script>
@@ -8,3 +13,13 @@
         name: "slot-child-component",
     };
 </script>
+
+<style>
+    #product-card {
+        width: 400px;
+        padding: 2px;
+        text-align: center;
+        border: 1px solid;
+        background-color: beige;
+    }
+</style>
