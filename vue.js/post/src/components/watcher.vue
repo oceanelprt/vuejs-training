cfcea3c9a3ca33594f3e60c5901024a61776637c
olaporte <oceane.laporte1@gmail.com>
2023-06-19 17:50:42 +0200

    Multiple slots

--- vue.js/post/src/components/watcher.vue
+++ vue.js/post/src/components/watcher.vue
@@ -6,7 +6,7 @@
     <button v-on:click="counter = counter + 1">+</button>
     <button v-on:click="counter = counter - 1">-</button>
 
-    <ChildSlotComponent>
+    <!-- <ChildSlotComponent>
     <h1>Hello there !</h1>
     </ChildSlotComponent>
     <ChildSlotComponent>
@@ -15,7 +15,21 @@
     <ChildSlotComponent>
         <img src="https://media.tenor.com/smu7cmwm4rYAAAAC/general-kenobi-kenobi.gif" alt="GIF Hello There General Kenobi"/>
     </ChildSlotComponent>
-</template>
+    <ChildSlotComponent></ChildSlotComponent> -->
+
+    <ChildSlotComponent>
+        <template v-slot:header><h1>First Heading</h1></template>
+        <template v-slot:section><h3>Product</h3></template>
+        <template v-slot:main><h3>Name</h3></template>
+        <template v-slot:footer><h2>Price</h2></template>
+    </ChildSlotComponent>
+
+    <ChildSlotComponent>
+        <template v-slot:header><h1>Brand</h1></template>
+        <template v-slot:section><h3>Product</h3></template>
+        <template v-slot:main><h3>Name</h3></template>
+        <template v-slot:footer><h2>Price</h2></template>
+    </ChildSlotComponent></template>
 
 <script>
     import ChildSlotComponent from './child-slot.vue'
